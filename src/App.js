91ebfc6f23cbb5023ccb4cafb74c504b29fe06ac
Paweł Szczerbicki <szczerbicki.pawel@gmail.com>
2022-01-24 17:25:13 +0100

    iit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useAuth0} from "@auth0/auth0-react";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [token, setToken] = useState();
+  const {getAccessTokenSilently, user} = useAuth0()
+
+  useEffect(() => {
+    getAccessTokenSilently()
+      .then(t => setToken(t))
+      .catch(console.log)
+  }, [setToken, getAccessTokenSilently])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello, I'm argo in ReactJS</p>
+        <p>My token is:  {token},</p>
+        <p>user is: {JSON.stringify(user)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Auth0Provider
+          domain="medcase-dev.eu.auth0.com"
+          clientId="gHbI6tWZ3dRUmH6yIFXfCXuUCsfdWH3R"
+      >
+          <App />
+      </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
